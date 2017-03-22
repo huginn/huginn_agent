6a68151fc2890639d8ece3b2315b3f4c00c10703
Dominik Sander <git@dsander.de>
2017-03-22 22:31:48 +0100

    Use `system` to run Agent gem specs
    This allows the usage of gems like `pry` when running the specs.

--- spec/lib/huginn_agent/helper_spec.rb
+++ spec/lib/huginn_agent/helper_spec.rb
@@ -4,28 +4,34 @@ describe HuginnAgent::Helper do
   context '#open3' do
     it "returns the exit status and output of the command" do
       expect(HuginnAgent::Helper).not_to receive(:print)
-      (status, output) = HuginnAgent::Helper.open3("pwd", false)
-      expect(status).to eq(0)
-      expect(output).to eq("#{Dir.pwd}\n")
-    end
-
-    it "prints the output directly when specified" do
-      expect(HuginnAgent::Helper).to receive(:print).twice
-      (status, output) = HuginnAgent::Helper.open3("pwd", true)
+      (status, output) = HuginnAgent::Helper.open3("pwd")
       expect(status).to eq(0)
       expect(output).to eq("#{Dir.pwd}\n")
     end
 
     it "return 1 as the status for failing command" do
-      (status, output) = HuginnAgent::Helper.open3("false", false)
+      (status, output) = HuginnAgent::Helper.open3("false")
       expect(status).to eq(1)
     end
 
     it "returns 1 when an IOError occurred" do
       expect(IO).to receive(:select).and_raise(IOError)
-      (status, output) = HuginnAgent::Helper.open3("pwd", false)
+      (status, output) = HuginnAgent::Helper.open3("pwd")
       expect(status).to eq(1)
       expect(output).to eq('IOError IOError')
+    end
+  end
+
+  context '#exec' do
+    it "returns the exit status and output of the command" do
+      (status, output) = HuginnAgent::Helper.exec("pwd")
+      expect(status).to eq(0)
+      expect(output).to eq('')
+    end
+
+    it "return 1 as the status for failing command" do
+      (status, output) = HuginnAgent::Helper.exec("false")
+      expect(status).to eq(1)
     end
   end
 end
